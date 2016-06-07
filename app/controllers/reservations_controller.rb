ebbe8e29479e6a02a2c209a52fd43b742409b6c4
Milou Schelling <milou.schelling@gmail.com>
2016-06-07 17:55:28 +0200

    add new and create to reservationscontroller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,18 @@
 class ReservationsController < ApplicationController
+  def new
+    @reservation = Reservation.new
+  end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+    @reservation.save!
+    redirect_to reservation_path(@reservation)
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:user_id, :status, :message)
+  end
+
 end
