cd7d942c527f81c8802f922331fdb9a8e2c27b3b
Jay Yoon <jay.yoon7@gmail.com>
2016-06-09 11:13:53 +0200

    problem solved - sqlite to postgrsl

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160608113924) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "events", force: :cascade do |t|
     t.date     "date"
     t.integer  "user_id"
@@ -25,7 +28,7 @@ ActiveRecord::Schema.define(version: 20160608113924) do
     t.datetime "updated_at",  null: false
   end
 
-  add_index "events", ["user_id"], name: "index_events_on_user_id"
+  add_index "events", ["user_id"], name: "index_events_on_user_id", using: :btree
 
   create_table "reservations", force: :cascade do |t|
     t.integer  "user_id"
@@ -36,8 +39,8 @@ ActiveRecord::Schema.define(version: 20160608113924) do
     t.datetime "updated_at",                     null: false
   end
 
-  add_index "reservations", ["event_id"], name: "index_reservations_on_event_id"
-  add_index "reservations", ["user_id"], name: "index_reservations_on_user_id"
+  add_index "reservations", ["event_id"], name: "index_reservations_on_event_id", using: :btree
+  add_index "reservations", ["user_id"], name: "index_reservations_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -63,7 +66,10 @@ ActiveRecord::Schema.define(version: 20160608113924) do
     t.datetime "token_expiry"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "events", "users"
+  add_foreign_key "reservations", "events"
+  add_foreign_key "reservations", "users"
 end
