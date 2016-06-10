25cefbb5c6694d75c8b7e1117097abf546d35a4d
Milou Schelling <milou.schelling@gmail.com>
2016-06-10 11:32:05 +0200

    add geomap to the events search results

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160609093644) do
+ActiveRecord::Schema.define(version: 20160610083548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,9 @@ ActiveRecord::Schema.define(version: 20160609093644) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.float    "latitude"
+    t.float    "longitude"
+    t.string   "streetname"
   end
 
   add_index "events", ["user_id"], name: "index_events_on_user_id", using: :btree
